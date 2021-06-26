6296bfbb708f8deb9af2739164d2a6bcd16e7284
Julio Moreno <julioc.morenob@gmail.com>
2021-06-26 12:28:51 -0400

    Procfile added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-var pgpLib= require('pg-promise');
+var pgp = require('pg-promise')();
+var db = pgp(process.env.DATABASE_URL || {database: 'likeypixdb'});
 
 app.use(bodyParser.urlencoded);
+
+var PORT = process.env.PORT || 8000;
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
+
