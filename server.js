c4e1de30dcfdfa2feeb9ddcc641d6987e4ab547d
Julio Moreno <julioc.morenob@gmail.com>
2021-06-29 21:22:42 -0400

    Step 1 Done

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var bodyParser = require('body-parser');
 var pgp = require('pg-promise')();
 var db = pgp(process.env.DATABASE_URL || {database: 'likeypixdb'});
 
-app.use(bodyParser.urlencoded);
+// app.use(bodyParser.urlencoded);
+app.use(express.static('public'));
+
+app.get('/search?searchTerm=AmericanCuisine',function(req, res){
+  res.send("tagID is set to " + req.params.name);
+});
 
 var PORT = process.env.PORT || 8000;
 app.listen(PORT, function () {
